e57c54bc2d4b43b4da8f24b23a5ca68d7e22801e
Вадя <vadim.filatov.02@gmail.com>
2024-09-29 13:37:27 +0700

    Сделал дз

---
+++ src/Book.java
@@ -0,0 +1,39 @@
+public class Book {
+    private String name;
+    private int yearRelease;
+    private Author author;
+
+    public Book(String name, int yearRelease, Author author) {
+        this.name = name;
+        this.yearRelease = yearRelease;
+        this.author = author;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getYearRelease() {
+        return this.yearRelease;
+    }
+
+    public void setYearRelease(int yearRelease) {
+        this.yearRelease = yearRelease;
+    }
+    public Author getAuthor() {
+        return this.author;
+    }
+    public String toString(){
+        return "" + this.name + "" + yearRelease;
+    }
+    public boolean equals(Object other) {
+        if (other == this) return true;
+        if (other == null || other.getClass() != getClass()) return false;
+        Book otherBook = (Book) other;
+        return name.equals(otherBook.name) &&
+                author.equals(otherBook.author) &&
+                yearRelease == otherBook.yearRelease;
+    }
+
+
+}
